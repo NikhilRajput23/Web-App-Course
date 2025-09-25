073e75bbbbc0b4c93f47513d469e16a9753adbd7
YourGitHubUsername <nikhilrajput3006@gmail.com>
2025-09-25 22:56:48 +0530

    f

---
+++ function1.2.js
@@ -0,0 +1,13 @@
+function cal(a,b,arith){
+function arith(a,b){
+    return a+b;
+}
+const ans=arith(a,b);
+console.log(ans);
+}
+function sum(a,b){
+    return a+b;
+
+}
+const val=cal(1,2,sum);
+console.log(val)
---
+++ function1.js
@@ -0,0 +1,13 @@
+function user( num1,num2,fnttoAll){
+    let r=num1+num2;
+ fnttoAll(r);
+}
+function display(data){
+    console.log("Result is "+data);
+}
+function d(data){
+        console.log("sum of Result is "+data);
+
+}
+const value=user(1,2,d);
+
