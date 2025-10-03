cf1e41e5ad397f76f9b8962106731d3f0a47aa06
YourGitHubUsername <nikhilrajput3006@gmail.com>
2025-10-03 19:41:35 +0530

    adddate_json

---
+++ date.js
@@ -0,0 +1,4 @@
+function clear(){
+    console.log(new   Date().getTime());
+}
+setInterval(clear,1000);
---
+++ json.js
@@ -0,0 +1,11 @@
+const users='{"name":"nikhil","age":"24","gender":"Male"}';
+const uses=JSON.parse(users)
+console.log(uses.gender) 
+
+const us={
+    name1:"nil",
+    gender1:"male"
+
+};
+const final=JSON.stringify(us);
+console.log(final)
