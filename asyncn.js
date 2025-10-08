dfceab73ec1f868613e48b5f63773912e3245ad0
Nikhil Rajput <nikhirajput3006@gmail.com>
2025-10-08 21:08:15 +0530

    add

---
+++ asyncn.js
@@ -0,0 +1,24 @@
+function findsum(n){
+    let ans=0;
+for (let i = 0; i < n; i++) {
+  ans += i;
+}
+return ans;
+}
+
+function findSumTill100() {
+console.log(findsum(100));}
+
+// busy waiting
+function syncSleep() {
+  let a = 1;
+  for (let i = 0; i < 1000000000; i++) {
+    a++;
+  }
+}
+
+// syncSleep();
+// findSumTill100();
+setTimeout(findSumTill100, 2000)
+
+console.log("hello world");
